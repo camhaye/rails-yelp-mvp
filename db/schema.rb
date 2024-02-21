a967da645d814730a29672eac000668b492ab7d3
Camille Haye <ca.haye@outlook.fr>
2024-02-21 12:47:47 +0100

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_21_093744) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_21_103540) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+100.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    phone_number: Faker::PhoneNumber.phone_number,
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "belgian", "french", "japanese"].sample
+  )
+restaurant.save!
+end
